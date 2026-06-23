be9827c3b680027e0634fa0c816fe68fb756ec30
Nickyhub <nikolas.alexander.kaminsky@t-online.de>
2026-06-23 15:39:00 +0200

    Ready to start

--- post-build.bat
+++
@@ -1,7 +0,0 @@
-cd 
-
-xcopy "assets" "build/bin/assets" /h /i /c /k /e /r /y
-IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
-popd
-echo "Done."
-PAUSE
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,59 @@
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#define USE_GPU_ENGINE 0
+extern "C"
+{
+	__declspec(dllexport) unsigned long NvOptimusEnablement = USE_GPU_ENGINE;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = USE_GPU_ENGINE;
+}
+
+const short width = 1920, height = 1080;
+
 int main(void)
 {
-	// Initialization
-	//--------------------------------------------------------------------------------------
-	const int screenWidth = 1280;
-	const int screenHeight = 720;
 
+	if (!glfwInit())
+		return -1;
+
+	// enable opengl debugging output.
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // you might want to do this when testing the game for shipping
+
+	GLFWwindow *window = window = glfwCreateWindow(width, height, "Terraria", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// glfwSetKeyCallback(window, key_callback);
+
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(1);
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	// glDebugMessageCallback(glDebugOutput, 0);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glViewport(0, 0, width, height);
+		glClearColor(0.0f, 0.3f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
